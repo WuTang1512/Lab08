19ed941e45cc04cd3c2c9b18200f3370db46864f
WuTang1512 <118622894+WuTang1512@users.noreply.github.com>
2025-10-30 19:47:04 -0600

    Created a setUp() function that runs before every testFunction that initializes a `CustomList list` with city objects

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -1,14 +1,31 @@
 package com.example.lab08;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
 public class CustomListTest {
+    private CustomList list;
+    private City calgary;
+    private City edmonton;
+    private City vancouver;
+
+
+    @BeforeEach
+    public void setUp() {
+        list = new CustomList();
+
+        calgary = new City("Calgary", "AB");
+        edmonton = new City("Edmonton", "AB");
+        vancouver = new City("Vancouver", "BC");
+
+        list.addCity(calgary);
+        list.addCity(edmonton);
+        list.addCity(vancouver);
+    }
+
     @Test
     public void testHasCity() {
-        CustomList list = new CustomList();
-        City calgary = new City("Calgary", "AB");
-        list.addCity(calgary);
-
         assertTrue(list.hasCity(calgary));
     }
 }
